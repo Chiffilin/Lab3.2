973c0fae6d75dd1ac5b7c9926cfa13af01f325f2
Chiffilin <dimaaitbaev42@gmail.com>
2020-03-25 11:36:50 +0200

    Добавьте файлы проекта.

---
+++ Lab3.2/Lab3.2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    setlocale(0, "rus");
+    string str;
+    cout << "Введите массив из символов: ";
+    getline(cin, str);
+    for (size_t i = str.find("<i>"); i != string::npos; i = str.find("<i>", i))
+    {
+        str.insert(i + 1, "/");
+        i += 4;
+       
+    }
+    cout << "После замены: " << str<< endl;
+    return 0;
+}
